678f63122d0e005fa7a6d66f904bf4e6bead5156
Alexander <alexandersuntaxi20030414@gmail.com>
2024-05-14 12:10:16 -0500

    Create ErrorController

---
+++ Consulta_por_cedulas/Controllers/ErrorController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Consulta_por_cedulas.Controllers
+{
+    public class ErrorController : Controller
+    {
+        // GET: Error
+        public ActionResult NotFound()
+        {
+            TempData["Mensaje"] = "No tiene autorización para acceder a esta página.";
+            return View();
+        }
+    }
+}
